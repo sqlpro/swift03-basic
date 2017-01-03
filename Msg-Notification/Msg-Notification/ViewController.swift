8e2cd2b33a11218ae8ec2d041da6fc0a21baaa07
꼼꼼한 재은씨 <sqlpro@naver.com>
2017-01-03 19:12:38 +0900

    Update ViewController.swift

--- Msg-Notification/Msg-Notification/ViewController.swift
+++ Msg-Notification/Msg-Notification/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
         // 알림 설정 내용을 확인
         let setting = UIApplication.shared.currentUserNotificationSettings
         guard setting?.types != .none else {
-            let alert = UIAlertController(title:"알림 등록", message:"알림이 허용되어 있지 않습니다", preferredStyle : .alert)
+            let alert = UIAlertController(title: "알림 등록", message: "알림이 허용되어 있지 않습니다", preferredStyle: .alert)
             let ok = UIAlertAction(title: "확인", style: .default)
             alert.addAction(ok)
             
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
                 (_) in
                 
                 // 발송 완료 안내 메시지 창
-                let date = self.datepicker.date.addingTimeInterval(9*60*60)
+                let date = self.datepicker.date.addingTimeInterval(9 * 60 * 60)
                 let message = "알림이 등록되었습니다. 등록된 알림은 \(date)에 발송됩니다"
                 
                 let alert = UIAlertController(title: "알림등록", message: message, preferredStyle: .alert)
@@ -64,7 +64,7 @@ class ViewController: UIViewController {
             UIApplication.shared.scheduleLocalNotification(noti)
             
             // 발송 완료 안내 메시지 창
-            let date = self.datepicker.date.addingTimeInterval(9*60*60)
+            let date = self.datepicker.date.addingTimeInterval(9 * 60 * 60)
             let message = "알림이 등록되었습니다. 등록된 알림은 \(date)에 발송됩니다"
             
             let alert = UIAlertController(title: "알림등록", message: message, preferredStyle: .alert)
