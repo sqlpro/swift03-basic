f8a9f24844ddb44398011f66ff9d7d0757ba03fe
꼼꼼한 재은씨 <sqlpro@naver.com>
2017-01-03 19:15:13 +0900

    Update ViewController.swift

--- Msg-AlertController/Msg-AlertController/ViewController.swift
+++ Msg-AlertController/Msg-AlertController/ViewController.swift
@@ -5,11 +5,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var result : UILabel!
+    @IBOutlet var result: UILabel!
     
     override func viewDidLoad() {
-        // 메시지 창을 처리하기 부적절한 위치 (아직 뷰가 화면에 구현되기 전임)
-        
+        // 메시지 창을 처리하기 부적절한 위치 (아직 뷰가 화면에 구현되기 전임)        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -21,26 +20,22 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: "선택", message: "항목을 선택해주세요", preferredStyle: .alert)
         
         // 취소 버튼
-        let cancel = UIAlertAction(title: "취소", style: .cancel) {
-            (_) in
+        let cancel = UIAlertAction(title: "취소", style: .cancel) {(_) in
             self.result.text = "취소 버튼을 클릭했습니다"
         }
         
         // 확인 버튼 
-        let ok = UIAlertAction(title: "확인", style: .default) {
-            (_) in
+        let ok = UIAlertAction(title: "확인", style: .default) {(_) in
             self.result.text = "확인 버튼을 클릭했습니다"
         }
         
         // 실행 버튼 
-        let exec = UIAlertAction(title: "실행", style: .destructive) {
-            (_) in
+        let exec = UIAlertAction(title: "실행", style: .destructive) {(_) in
             self.result.text = "실행 버튼을 클릭했습니다"
         }
         
         // 중지 버튼 
-        let stop = UIAlertAction(title: "중지", style: .default) {
-            (_) in
+        let stop = UIAlertAction(title: "중지", style: .default) {(_) in
             self.result.text = "중지 버튼을 클릭했습니다"
         }
         
@@ -61,11 +56,11 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let cancel = UIAlertAction(title: "취소", style: .cancel)
-        let ok = UIAlertAction(title:"확인", style: .default) {
+        let ok = UIAlertAction(title: "확인", style: .default) {
             (_) in
             // 확인 버튼을 클릭했을 때 처리할 내용
             if let tf = alert.textFields?.first {
-                print("입력된 값은 \( tf.text! )입니다. ")
+                print("입력된 값은 \(tf.text!)입니다. ")
             } else {
                 print("입력된 값이 없습니다.")
             }
@@ -84,12 +79,12 @@ class ViewController: UIViewController {
         self.present(alert, animated:false)
     }
     
-    @IBAction func auth(_ sender : AnyObject) {
+    @IBAction func auth(_ sender: AnyObject) {
         let msg = "로그인"
         let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
         
         let cancel = UIAlertAction(title: "취소", style: .cancel)
-        let ok = UIAlertAction(title:"확인", style: .default) {
+        let ok = UIAlertAction(title: "확인", style: .default) {
             (_) in
             // 확인 버튼을 클릭했을 때 처리할 내용
             let loginId = alert.textFields?[0].text
@@ -106,7 +101,7 @@ class ViewController: UIViewController {
         alert.addAction(ok)
         
         // 텍스트 필드 추가 - 아이디
-        alert.addTextField(configurationHandler: { (tf) in
+        alert.addTextField(configurationHandler: {(tf) in
             // 텍스트필드의 속성설정
             tf.placeholder = "아이디" // 안내 메세지
             tf.isSecureTextEntry = false // 비밀번호 처리 안함
@@ -119,7 +114,7 @@ class ViewController: UIViewController {
             tf.isSecureTextEntry = true // 비밀번호 처리
         })
         
-        self.present(alert, animated:false)
+        self.present(alert, animated: false)
         
         alert.addTextField() {
             $0.placeholder = "비밀번호"
